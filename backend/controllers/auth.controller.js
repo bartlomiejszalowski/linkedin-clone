14f7e7c6a5f2861667282e8875eb5624c3bc3b33
Bartek <bartlomiejszalowski@gmail.com>
2024-10-09 12:52:11 +0200

    welcome email created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
   try {
@@ -62,7 +63,13 @@ export const signup = async (req, res) => {
       message: "User registered successfully",
     });
 
-    //todo: send welcome email
+    const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+
+    try {
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
+    } catch (emailError) {
+      console.log("Error in sending welcome email:", emailError);
+    }
   } catch (error) {
     console.log("Error in signup:", error.message);
     res.status(500).json({ message: "Internal server error" });
