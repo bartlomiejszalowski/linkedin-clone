5c87bb6141e710b2459d2f266d7cfdf1cf74a24a
Bartek <bartlomiejszalowski@gmail.com>
2024-10-10 14:21:00 +0200

    user routes created

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
