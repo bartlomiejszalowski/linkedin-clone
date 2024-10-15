107ddac53f8aeea2d8f871418552935035488841
Bartek <bartlomiejszalowski@gmail.com>
2024-10-15 11:14:11 +0200

    created frontend setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -16,6 +17,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow session cookie from browser to pass through
+  })
+);
 app.use(express.json({ limit: "5mb" })); // to accept json
 app.use(cookieParser()); // to accept cookies
 
