21ec4163ca480f232e7841fdf1740ac6d43aa40f
Bartek <bartlomiejszalowski@gmail.com>
2024-10-17 18:34:38 +0200

    created signup page and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,35 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
+import { Toaster } from "react-hot-toast";
+import useGetAuthUser from "./hooks/useGetAuthUser";
 
 function App() {
+  const { authUser, isLoading, error } = useGetAuthUser();
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
+      <Toaster />
     </Layout>
   );
 }
